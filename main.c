0f3ac5b422f9f5a90314c25a289ab187960a8802
Kim Minwoo <davidkmw0810@gmail.com>
2019-04-22 23:29:32 +0900

    create makefile

--- main.c
+++ main.c
@@ -3,18 +3,26 @@
 # include<stdio.h>
 
 int main(int argc, char *argv[]){
+    printf("You can use linux function, and login, makec, socketc command\n");
     while(1){
+
+	    printf(">>");
         // Check anything write
 		if(argc == 0){
             continue;
         }
 
 		switch (argv[0]){
-            case "exit" :
+            case "exit" : 
+			    return 0;
 			case "login" :
+			    login(argv);
 			case "makec" :
+			    makec(argv);
 			case "socketc" :
-			default :
+			    socketc(argv);
+			default : // for normal function
+			    function(argv);
 		}
 
     }
---
+++ makefile
@@ -0,0 +1,19 @@
+CC = gcc
+STRIP = strip
+
+SRC1 = main
+SRC2 = function
+
+myshell : $(SRC1).o $(SRC2).o
+	$(CC) -o myshell $(SRC1).o $(SRC2).o
+	$(STRIP) myshell
+
+$(SRC1).o : $(SRC1).c
+	$(CC) -c $(SRC1).o $(SRC1).c
+
+
+$(SRC2).o : $(SRC2).c
+	$(CC) -c $(SRC2).o $(SRC2).c
+
+clean :
+	rm -f *.o 
