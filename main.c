fa9a8e9ccbc1ca873b90e2741ec4986500d142c1
Kim Minwoo <davidkmw0810@gmail.com>
2019-04-23 19:59:55 +0900

    rewrite Makefile function.c main.c

--- main.c
+++ main.c
@@ -1,30 +1,38 @@
 // Main Controller
 
-# include<stdio.h>
+# include <stdio.h>
+# include "function.c"
+# include "login.c"
+# include "makec.c"
+# include "socketc.c"
+# define max_len 100
 
 int main(int argc, char *argv[]){
     printf("You can use linux function, and login, makec, socketc command\n");
     while(1){
 
 	    printf(">>");
+
+        // get command from user
+		char str[max_len];
+		fgets(str, max_len, stdin);
+		
         // Check anything write
-		if(argc == 0){
+		if(str == 0){
             continue;
         }
 
-		if (argv[0] == "exit"){
+		if (strcmp(str, "exit") == 0){
 		    return 0;
-		} else if(argv[0] == "login"){
-		    login(argv);
-		} else if(argv[0] == "makec"){
-		    makec(argv);
-		} else if(argv[0] == "socketc"){
-		    socketc(argv);
+		} else if(strcmp(str, "login") == 0){
+		    login();
+		} else if(strcmp(str, "makec") == 0){
+		    makec();
+		} else if(strcmp(str, "socketc") == 0){
+		    socketc();
 		} else {
-		    function(argv);
+		    function(str);
 		}
-
-
 
     }
     return 0;
